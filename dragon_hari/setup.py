1d302afc58213e85ec0bbd15c6fae6fa6a280cc8
Tsukazaki Kyoji <tsukazaki@dragon.t.u-tokyo.ac.jp>
2026-08-03 17:21:53 +0900

    Add Python package structure for dragon_hari

---
+++ dragon_hari/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+
+setup_args = generate_distutils_setup(
+    packages=["dragon_hari"],
+    package_dir={"": "src"},
+)
+
+setup(**setup_args)
